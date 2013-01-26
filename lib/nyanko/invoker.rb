8a0813e47e0fabf6bb4517b0ded6ebba80bb1371
Ryo Nakamura <r7kamura@gmail.com>
2013-01-26 22:33:59 +0900

    Add locals option feature to invoker
    In invoke phase, passed locals options are stacked to `unit_locals_stack`,
    and accessed via method_missing.

--- lib/nyanko/invoker.rb
+++ lib/nyanko/invoker.rb
@@ -6,7 +6,24 @@ module Nyanko
     def invoke(*args)
       options  = Options.new(*args)
       function = FunctionFinder.find(self, options)
+      unit_locals_stack.push(options.locals)
       function.invoke(self, options.invoke_options) if function
+    ensure
+      unit_locals_stack.pop
+    end
+
+    private
+
+    def method_missing(method_name, *args, &block)
+      if args.empty? && value = (unit_locals_stack.last || {})[method_name]
+        value
+      else
+        super
+      end
+    end
+
+    def unit_locals_stack
+      @unit_locals_stack ||= []
     end
   end
 end
--- spec/nyanko/invoker_spec.rb
+++ spec/nyanko/invoker_spec.rb
@@ -3,17 +3,10 @@ require "spec_helper"
 module Nyanko
   describe Invoker do
     before do
-      Loader.stub(:fetch) do |unit_name|
-        case unit_name
-        when :unit_a
-          unit_a
-        when :unit_b
-          unit_b
-        end
-      end
+      Loader.stub(:fetch) {|unit_name| send(unit_name) }
     end
 
-    let(:unit_a) do
+    let(:unit) do
       Module.new do
         include Nyanko::Unit
 
@@ -25,11 +18,15 @@ module Nyanko
           function(:self) do
             self
           end
+
+          function(:locals) do
+            key
+          end
         end
       end
     end
 
-    let(:unit_b) do
+    let(:inactive_unit) do
       Module.new do
         include Nyanko::Unit
         active_if { false }
@@ -42,16 +39,25 @@ module Nyanko
 
     describe "#invoke" do
       it "invokes defined function for current context" do
-        view.invoke(:unit_a, :test).should == "test"
+        view.invoke(:unit, :test).should == "test"
       end
 
       it "invokes in the same context with receiver" do
-        view.invoke(:unit_a, :self).should == view
+        view.invoke(:unit, :self).should == view
+      end
+
+      it "invokes with locals option" do
+        expect { view.invoke(:unit, :locals) }.to raise_error(NameError)
+        view.invoke(:unit, :locals, :locals => { :key => "value" }).should == "value"
+      end
+
+      it "invokes with short-hand style locals option" do
+        view.invoke(:unit, :locals, :key => "value").should == "value"
       end
 
       context "when dependent unit is inactive" do
         it "does nothing" do
-          view.invoke(:unit_a, :test, :if => :unit_b).should == nil
+          view.invoke(:unit, :test, :if => :inactive_unit).should == nil
         end
       end
     end
