a0e534344dfd76cc875858ed050f186d92ab7f78
Ryo Nakamura <r7kamura@gmail.com>
2013-01-28 01:52:48 +0900

    Add Railtie for Rails to load Nyanko

---
+++ lib/nyanko/railtie.rb
@@ -0,0 +1,13 @@
+require "rails"
+
+module Nyanko
+  class Railtie < Rails::Railtie
+    initializer "nyanko" do |app|
+      ::ActionView::Base.send(:include, Invoker, Helper, UnitProxyProvider)
+      ::ActionController::Base.send(:include, Controller)
+      ::ActiveRecord::Base.send(:include, UnitProxyProvider)
+      ::ActiveRecord::Relation.send(:include, UnitProxyProvider)
+      ::ActiveRecord::Associations::CollectionAssociation.send(:include, UnitProxyProvider)
+    end
+  end
+end
